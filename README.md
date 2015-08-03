a8cb5e45526901de30c0f2f1c73eca01254a6e9e
Shaun Miller <binarypearl@gmail.com>
2015-08-02 20:26:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Testing out Jenkins with c++ and github
--- t1.cpp
+++ t1.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main() {
 	cout << "Hi there from Jenkins test." << endl;	
 
+	cout << "missing semi colong." << endl
+
 	return (0);
 }
